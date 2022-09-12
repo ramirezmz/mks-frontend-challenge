355e1829b473ad2b343b88e9966a86344feefece
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-12 01:06:26 -0300

    feat: create footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {FooterDiv} from './styled'
+
+const Footer = () => {
+  return (
+    <FooterDiv>
+      <p>MKS sistemas © Todos os direitos reservados</p>
+    </FooterDiv>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const FooterDiv = styled.footer`
+  background-color: #EEEEEE;
+  text-align: center;
+  width: 100%;
+  height: auto;
+  margin: auto;
+  bottom: 0;
+  padding: 0.4rem;
+  position: fixed;
+`
