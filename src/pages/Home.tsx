a39aef8814e5ee7fa8208b246a3e3cd760053734
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-12 00:40:02 -0300

    feat: add card component to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,27 @@
 import React, { useEffect } from 'react'
 import Header from '../components/Header/Header'
+import Card from '../components/Card/Card'
 import api from '../services/api'
+import {ContentItems} from './styled'
 
 const Home = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      const url = 'products?page=1&rows=8&sortBy=id&orderBy=DESC'
-      const result = await api.get(url)
-      console.log(result.data)
-    }
-    // fetchData()
-  })
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const url = 'products?page=1&rows=8&sortBy=id&orderBy=DESC'
+  //     const result = await api.get(url)
+  //     console.log(result.data)
+  //   }
+  //   fetchData()
+  // })
   return (
     <div>
       <Header />
+      <ContentItems>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </ContentItems>
     </div>
   )
 }
---
+++ src/pages/styled.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const ContentItems = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 7rem;
+`
