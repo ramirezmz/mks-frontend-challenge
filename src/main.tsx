08cc7f75a7428d5e519dfc1d1da4f4ba7601ce55
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-13 07:29:23 -0300

    feat: add config redux on main

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { GlobalStyle } from './styled'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import storeReducer from './features/Store'
+
+const store = configureStore({
+  reducer: {
+    items: storeReducer,
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>
 )
