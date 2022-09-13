81aa5c457fae1e14344d921f24788032000e2ad1
ramirezmz <robertopaoloramirezmunoz7@gmail.com>
2022-09-13 11:26:51 -0300

    feat: add remove product function

--- src/features/Store.ts
+++ src/features/Store.ts
@@ -10,6 +10,9 @@ export const userSlice = createSlice({
     addProduct: (state, action) => {
       state.value.push(action.payload)
     },
+    removeProduct: (state, action) => {
+      state.value = state.value.filter((user) => user.id !==action.payload.id)
+    },
     handleSidebar: (state, action) => {
       state.sidebar = action.payload
       console.log(action.payload)
@@ -17,6 +20,6 @@ export const userSlice = createSlice({
   }
 })
 
-export const {addProduct, handleSidebar} = userSlice.actions
+export const {addProduct, handleSidebar, removeProduct} = userSlice.actions
 
 export default userSlice.reducer
